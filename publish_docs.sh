54ff3715dfd2aae7386f10229053eb4bcf38887b
Marko Ilic <ilic.marko.05@live.com>
2020-06-22 20:35:16 +0200

    update gh-pages script

--- publish_docs.sh
+++ publish_docs.sh
@@ -8,12 +8,8 @@ cd ../
 # checkout to the gh-pages branch
 git checkout gh-pages
 
-# pull the latest updates
-git pull origin gh-pages --rebase
-
 # copy the static site files into the current directory.
 cp -R docs/_book/* ./
-
 
 # remove 'node_modules' and '_book' directory
 git clean -fx node_modules
